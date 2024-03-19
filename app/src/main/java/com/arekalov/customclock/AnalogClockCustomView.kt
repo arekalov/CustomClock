d2d4b46e894e0a50420a4615267b307ba6f367f1
arekalov <artyom.rekalov@gmail.com>
2024-03-19 21:39:21 +0300

    add attributes to AnalogClockCustomView.kt

--- app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
+++ app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
@@ -31,12 +31,12 @@ class AnalogClockCustomView @JvmOverloads constructor(
     private lateinit var paintDigit: Paint
     private lateinit var paintBackground: Paint
     private var backgroundColor = 0xFFE8E4E1.toInt()
-    private var digitColor =  Color.BLACK
-    private var borderColor =  Color.BLACK
+    private var digitColor = Color.BLACK
+    private var borderColor = Color.BLACK
     private var borderWidth = DEFAULT_BORDER
-    private var secondHandColor =  Color.BLACK
-    private var minuteHandColor =  Color.BLACK
-    private var hourHandColor =  Color.BLACK
+    private var secondHandColor = Color.BLACK
+    private var minuteHandColor = Color.BLACK
+    private var hourHandColor = Color.BLACK
     private var isSecondHandVisible = true
     private var numbers = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
     private var SIZE_DP = TypedValue.applyDimension(
@@ -47,57 +47,84 @@ class AnalogClockCustomView @JvmOverloads constructor(
     ).toInt()
     private var radius = width / 2F - BOARDER_PX
     private var center = width / 2F
-//    init {
-//        if (attrs != null) {
-//            val attrsSet = context.obtainStyledAttributes(attrs, R.styleable.MyClock)
-//            backgroundColor = attrsSet.getColor(R.styleable.MyClock_backgroundColor, backgroundColor)
-//            digitColor = attrsSet.getColor(R.styleable.MyClock_digitsColor, digitColor)
-//            borderColor = attrsSet.getColor(R.styleable.MyClock_borderColor, borderColor)
-//            borderWidth = attrsSet.getInteger(R.styleable.MyClock_borderWidth, borderWidth)
-//            secondHandColor = attrsSet.getColor(R.styleable.MyClock_secondHandColor, secondHandColor)
-//            minuteHandColor = attrsSet.getColor(R.styleable.MyClock_minuteHandColor, minuteHandColor)
-//            hourHandColor = attrsSet.getColor(R.styleable.MyClock_hourHandColor, hourHandColor)
-//            isSecondHandVisible = attrsSet.getBoolean(R.styleable.MyClock_isSecondHandVisible, isSecondHandVisible)
-//        }
-//    }
-private fun initPainters() {
-    paintBackground = Paint().apply {
-        color = backgroundColor
-        Paint.Style.FILL
-        isAntiAlias = true
-    }
-    paintBorder = Paint().apply {
-        color = borderColor
-        style = Paint.Style.STROKE
-        strokeWidth = BOARDER_PX.toFloat()
-        isAntiAlias = true
-    }
-    paintDigit = Paint().apply {
-        color = digitColor
-        Paint.Style.FILL
-        isAntiAlias = true
-        textSize = SIZE_DP.toFloat() / 20F
-    }
-    paintHourHand = Paint().apply {
-        color = hourHandColor
-        Paint.Style.FILL
-        isAntiAlias = true
-        strokeWidth = SIZE_DP * 0.01F
+
+    init {
+        if (attrs != null) {
+            val attrsSet = context.obtainStyledAttributes(attrs, R.styleable.AnalogClockCustomView)
+            backgroundColor = attrsSet.getColor(
+                R.styleable.AnalogClockCustomView_clockBackgroundColor,
+                backgroundColor
+            )
+            digitColor =
+                attrsSet.getColor(R.styleable.AnalogClockCustomView_clockDigitsColor, digitColor)
+            borderColor =
+                attrsSet.getColor(R.styleable.AnalogClockCustomView_clockBorderColor, borderColor)
+            borderWidth =
+                attrsSet.getInteger(
+                    R.styleable.AnalogClockCustomView_clockBorderWidth,
+                    borderWidth.toInt()
+                )
+                    .toFloat()
+            secondHandColor =
+                attrsSet.getColor(
+                    R.styleable.AnalogClockCustomView_clockSecondHandColor,
+                    secondHandColor
+                )
+            minuteHandColor =
+                attrsSet.getColor(
+                    R.styleable.AnalogClockCustomView_clockMinuteHandColor,
+                    minuteHandColor
+                )
+            hourHandColor = attrsSet.getColor(
+                R.styleable.AnalogClockCustomView_clockHourHandColor,
+                hourHandColor
+            )
+            isSecondHandVisible =
+                attrsSet.getBoolean(
+                    R.styleable.AnalogClockCustomView_clockIsSecondHandVisible,
+                    isSecondHandVisible
+                )
+        }
     }
 
-    paintMinuteHand = Paint().apply {
-        color = minuteHandColor
-        Paint.Style.FILL
-        isAntiAlias = true
-        strokeWidth = SIZE_DP * 0.005F
+    private fun initPainters() {
+        paintBackground = Paint().apply {
+            color = backgroundColor
+            Paint.Style.FILL
+            isAntiAlias = true
+        }
+        paintBorder = Paint().apply {
+            color = borderColor
+            style = Paint.Style.STROKE
+            strokeWidth = BOARDER_PX.toFloat()
+            isAntiAlias = true
+        }
+        paintDigit = Paint().apply {
+            color = digitColor
+            Paint.Style.FILL
+            isAntiAlias = true
+            textSize = SIZE_DP.toFloat() / 20F
+        }
+        paintHourHand = Paint().apply {
+            color = hourHandColor
+            Paint.Style.FILL
+            isAntiAlias = true
+            strokeWidth = SIZE_DP * 0.01F
+        }
+
+        paintMinuteHand = Paint().apply {
+            color = minuteHandColor
+            Paint.Style.FILL
+            isAntiAlias = true
+            strokeWidth = SIZE_DP * 0.005F
+        }
+        paintSecondHand = Paint().apply {
+            color = secondHandColor
+            Paint.Style.FILL
+            isAntiAlias = true
+            strokeWidth = SIZE_DP * 0.002F
+        }
     }
-    paintSecondHand = Paint().apply {
-        color = secondHandColor
-        Paint.Style.FILL
-        isAntiAlias = true
-        strokeWidth = SIZE_DP * 0.002F
-    }
-}
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val width = when (MeasureSpec.getMode(widthMeasureSpec)) {
@@ -160,7 +187,7 @@ private fun initPainters() {
         canvas.drawLine(
             width / 2f,
             width / 2f,
-            (width / 2f + cos(mAngle) * radius * 0.75 ).toFloat(),
+            (width / 2f + cos(mAngle) * radius * 0.75).toFloat(),
             (width / 2f + sin(mAngle) * radius * 0.75).toFloat(),
             paintSecondHand
         )
@@ -187,7 +214,8 @@ private fun initPainters() {
         for (number in numbers) {
             val num = number.toString()
             val angle = Math.PI / 6 * (number - 3)
-            val x = (width / 2 + cos(angle) * radius * 0.75 - (paintDigit.textSize * num.length) / 4).toFloat()
+            val x =
+                (width / 2 + cos(angle) * radius * 0.75 - (paintDigit.textSize * num.length) / 4).toFloat()
             val y = (width / 2 + sin(angle) * radius * 0.75 + paintDigit.textSize / 3).toFloat()
             canvas.drawText(num, x, y, paintDigit)
         }
