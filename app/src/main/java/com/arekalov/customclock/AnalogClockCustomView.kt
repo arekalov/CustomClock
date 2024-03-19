97dbd274a644b222651409184e3735cafaeb28cd
arekalov <artyom.rekalov@gmail.com>
2024-03-19 21:29:39 +0300

    add logic for onDraw in AnalogClockCustomView

--- app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
+++ app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
@@ -1,12 +1,16 @@
 package com.arekalov.customclock
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.icu.util.Calendar
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
+import kotlin.math.cos
 import kotlin.math.min
+import kotlin.math.sin
 
 class AnalogClockCustomView @JvmOverloads constructor(
     context: Context,
@@ -117,5 +121,76 @@ private fun initPainters() {
         center = size / 2f
         initPainters()
         setMeasuredDimension(size, size)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawDial(canvas)
+        drawDigits(canvas)
+        drawHands(canvas)
+        postInvalidateDelayed(1000)
+    }
+
+    private fun drawHands(canvas: Canvas) {
+        val calendar: Calendar = Calendar.getInstance()
+        var mHour = calendar.get(Calendar.HOUR_OF_DAY).toFloat()
+        mHour = if (mHour > 12) mHour - 12 else mHour
+        val mMinute = calendar.get(Calendar.MINUTE).toFloat()
+        val mSecond = calendar.get(Calendar.SECOND).toFloat()
+        drawHourHand(canvas, (mHour + mMinute / 60.0) * 5f)
+        drawMinuteHand(canvas, mMinute)
+        if (isSecondHandVisible) {
+            drawSecondsHand(canvas, mSecond)
+        }
+    }
+
+    private fun drawMinuteHand(canvas: Canvas, location: Float) {
+        val mAngle = Math.PI * location / 30 - Math.PI / 2
+        canvas.drawLine(
+            width / 2f,
+            width / 2f,
+            (width / 2f + cos(mAngle) * radius * 0.7).toFloat(),
+            (width / 2f + sin(mAngle) * radius * 0.7).toFloat(),
+            paintMinuteHand
+        )
+    }
+
+    private fun drawSecondsHand(canvas: Canvas, location: Float) {
+        val mAngle = Math.PI * location / 30 - Math.PI / 2
+        canvas.drawLine(
+            width / 2f,
+            width / 2f,
+            (width / 2f + cos(mAngle) * radius * 0.75 ).toFloat(),
+            (width / 2f + sin(mAngle) * radius * 0.75).toFloat(),
+            paintSecondHand
+        )
+    }
+
+    private fun drawHourHand(canvas: Canvas, location: Double) {
+        val mAngle = Math.PI * location / 30 - Math.PI / 2
+        canvas.drawLine(
+            width / 2f,
+            width / 2f,
+            (width / 2f + cos(mAngle) * radius * 0.6).toFloat(),
+            (width / 2f + sin(mAngle) * radius * 0.6).toFloat(),
+            paintHourHand
+        )
+    }
+
+    private fun drawDial(canvas: Canvas) {
+        canvas.drawCircle(width / 2F, width / 2F, radius, paintBackground)
+        canvas.drawCircle(width / 2f, width / 2f, radius, paintBorder)
+        canvas.drawCircle(width / 2f, width / 2f, DEFAULT_SIZE_DP * 0.008F, paintHourHand)
+    }
+
+    private fun drawDigits(canvas: Canvas) {
+        for (number in numbers) {
+            val num = number.toString()
+            val angle = Math.PI / 6 * (number - 3)
+            val x = (width / 2 + cos(angle) * radius * 0.75 - (paintDigit.textSize * num.length) / 4).toFloat()
+            val y = (width / 2 + sin(angle) * radius * 0.75 + paintDigit.textSize / 3).toFloat()
+            canvas.drawText(num, x, y, paintDigit)
+        }
+
     }
 }
