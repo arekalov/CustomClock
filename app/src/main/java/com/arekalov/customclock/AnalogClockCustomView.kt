51534edab2c4990c4a6020320527599f1417981a
arekalov <artyom.rekalov@gmail.com>
2024-03-19 21:26:43 +0300

    add constructor

--- app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
+++ app/src/main/java/com/arekalov/customclock/AnalogClockCustomView.kt
@@ -1,4 +1,13 @@
 package com.arekalov.customclock
 
-class AnalogClockCustomView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class AnalogClockCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defResAttrs: Int = 0
+) : View(context, attrs, defStyleAttr, defResAttrs) {
 }
