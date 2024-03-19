aba7cafbf9b8fe2618de57ced01e28d66e21d275
arekalov <artyom.rekalov@gmail.com>
2024-03-19 23:00:51 +0300

    add bottom navView to MainActivity

--- app/src/main/java/com/arekalov/customclock/MainActivity.kt
+++ app/src/main/java/com/arekalov/customclock/MainActivity.kt
@@ -5,10 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNavigation.setupWithNavController(navController)
     }
 }
